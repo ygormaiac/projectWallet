295994134c6addc3d3fba27ac8809889d7395b6f
ygormaiac <ygormaiac@users.noreply.github.com>
2021-09-14 18:52:16 +0000

    deploy: e39d8197f4993617462b899407bc1345e8220a11

---
+++ precache-manifest.e08c682dcd9a322cebb5d716b78eed10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0889bb47e803c4e580d86fc42ba444",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cd68d118bbd64eadaea",
+    "url": "/static/css/main.26e52db9.chunk.css"
+  },
+  {
+    "revision": "181ea1402160421eeab8",
+    "url": "/static/js/2.05e3fde9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.05e3fde9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd68d118bbd64eadaea",
+    "url": "/static/js/main.81bba829.chunk.js"
+  },
+  {
+    "revision": "fe617c2cb41aece9e721",
+    "url": "/static/js/runtime-main.e8df815d.js"
+  },
+  {
+    "revision": "cad8efa7851e14c7ddfc0a90e774d576",
+    "url": "/static/media/gifWallet.cad8efa7.gif"
+  }
+]);
